7b8da49a785cc43643527d1aca5c5b5d9196f322
ebubekirkarakurt <ebukar75@gmail.com>
2024-09-08 13:09:44 +0300

    loading circle added

--- src/features/homescreen/HomeScreen.tsx
+++ src/features/homescreen/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../../styles/navbar.styles.css';
 import '../../styles/homescren.style.css';
 import { ReactComponent as NoCityLogo } from '../../assets/no-city.svg';
@@ -11,61 +11,65 @@ import WeatherDetails from './components/WeatherDetails';
 import DetailContainer from './components/DetailContainer';
 import { RootState } from '../../redux/store/store';
 import { WeatherData } from '../../types/WeatherData';
+import LoadingCircle from './components/LoadingCircle'; // Import the LoadingCircle component
 
 type Props = {
-    data: WeatherData,
+  data: WeatherData,
+  isLoading: boolean
 }
 
-export default function HomeScreen({data}: Props) {
+export default function HomeScreen({ data, isLoading }: Props) {
 
   const dispatch = useAppDispatch();
-  
-  const [city, setCity] = useState<string>('')
+  const [city, setCity] = useState<string>('');
 
   function handleChangeText(e: React.ChangeEvent<HTMLInputElement>) {
-    setCity(e.target.value);   
-    dispatch(getCityName(e.target.value)); 
+    setCity(e.target.value);
+    dispatch(getCityName(e.target.value));
   }
 
-  const cityName = useAppSelector((state: RootState) => state.city.value)
+  const cityName = useAppSelector((state: RootState) => state.city.value);
 
   return (
     <div>
-    <div className='body'>
-    {
-            data && cityName  ? 
-            <WeatherTable data={data} />
-            :
-            data === null && cityName ? 
-            <InvalidLogo className='no-city-logo' /> 
-            : 
-            <NoCityLogo className='no-city-logo' /> 
-          }    
-      
-      <div className='container'>
-        <div className='input-container'>
-          <input 
-            name="myInput" 
-            id='input' 
-            placeholder='Search a City' 
-            onChange={handleChangeText}
-            value={city}
-          />
-          <InputIcon id='input-icon'/>
-        </div>
-        <div className="select-city-container">
-          {
-            data && cityName ? 
-             <WeatherDetails data={data} /> 
-            :
-            data === null && cityName ? 
-            <DetailContainer title='Does not Exist' description='Type a valid city name to get weekly forecast data.' />
-            :
-            <DetailContainer title='Select a City' description='Search and select a city to see results. Try typing the first letters of the city you want.' />
-          }            
+      <div className='body'>
+        {
+          isLoading ? 
+          <LoadingCircle /> 
+          :
+          data && cityName ? 
+          <WeatherTable data={data} />
+          :
+          data === null && cityName ? 
+          <InvalidLogo className='no-city-logo' />
+          : 
+          <NoCityLogo className='no-city-logo' />
+        }
+
+        <div className='container'>
+          <div className='input-container'>
+            <input 
+              name="myInput" 
+              id='input' 
+              placeholder='Search a City' 
+              onChange={handleChangeText}
+              value={city}
+            />
+            <InputIcon id='input-icon'/>
+          </div>
+          <div className="select-city-container">
+            {
+              data && cityName ? 
+              <WeatherDetails data={data} /> 
+              :
+              data === null && cityName ? 
+              <DetailContainer title='Does not Exist' description='Type a valid city name to get weekly forecast data.' />
+              :
+              <DetailContainer title='Select a City' description='Search and select a city to see results. Try typing the first letters of the city you want.' />
+            }
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
 }
---
+++ src/features/homescreen/components/LoadingCircle.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../../styles/loading.circle.style.css';
+
+const LoadingCircle: React.FC = () => {
+  return <div className="loading-circle"></div>;
+};
+
+export default LoadingCircle;

2226f7332e0833828aae17274a0914982d6cb1ca
ebubekirkarakurt <ebukar75@gmail.com>
2024-09-08 12:16:33 +0300

    api informations updated

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://api.weatherbit.io/v2.0/forecast'
-export const API_KEY = '9b380007253f4bcfbdcf13bcc9549ce7'
+export const API_KEY = '4e72dbbfecc144a383a0ebcc7fbdc1c5'
